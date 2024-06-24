5acb33f6c268b7a3e13f3c7f0378c9e62008559c
Colégio Provecto <informatica@colegioprovecto.com.br>
2024-06-24 13:14:29 -0300

    alteração de arquivos de conexão do banco de dados

--- util/db.js
+++ util/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    host: 'aws-0-us-east-1.pooler.supabase.com',
-    user: 'postgres.tfajhjjkayrdqlrlxnwl',
-    password: 'ERJAzYVszp1anz2b',
-    port: 5432,
-    database: 'postgres'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
 })
 
 pool.connect((erro, client, release) => {
